371aa2c71107293b8d5bae7e6da293c8499f708b
goni5847 <gonmonfon@gmail.com>
2025-10-27 14:25:28 +0000

    ç added

--- edu-pro/src/components/TeamsFeatures/index.tsx
+++ edu-pro/src/components/TeamsFeatures/index.tsx
@@ -7,46 +7,53 @@ interface AvatarItemI {
   name: string;
   imageRef: string;
   href: string;
+  description: string
 }
 
 const AvatarList: AvatarItemI[] = [
   {
-    name: "Marta Condeco",
+    name: "Marta Condeço",
     href: "https://github.com/mawich16",
     imageRef: "MC.jpeg",
+    description: "Front End Developer"
   },
   {
     name: "Goncalo Fonseca",
     href: "https://github.com/goni5847",
     imageRef: "GF.jpeg",
+    description: "DevOps"
   },
   {
     name: "Tiago Coelho",
     href: "https://github.com/TCoelhoUA",
     imageRef: "TC.jpeg",
+    description: "Front End Developer"
   },
   {
     name: "Pedro Martins",
     href: "https://github.com/pedroMPMartins",
     imageRef: "PM.jpeg",
+    description: "Back End Developer" 
   },
   {
     name: "Vasco Pereira",
     href: "https://github.com/AzelCora",
     imageRef: "VP.jpeg",
+    description: "Back End Developer"
   },
 ];
 
-function AvatarCard({ name, imageRef, href }: AvatarItemI) {
+function AvatarCard({ name, imageRef, href , description}: AvatarItemI) {
   return (
     <div className={clsx("col col--4")}>
       <div className={styles.wrapper}>
         <div className={styles.featureSvg}>
-          <img src={require(`/team/${imageRef}`).default} />
+          <img src={require(`/team/${imageRef}`).default}  />
         </div>
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{name}</Heading>
+        <Heading as="h6">{description}</Heading>
         <a href={href}>
           <Github />
         </a>
